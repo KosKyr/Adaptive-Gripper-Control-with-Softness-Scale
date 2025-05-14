157e8fccd0a2731212439f5929c727b2a9548671
Stathis Liapodimitris <139817338+stathis-liap@users.noreply.github.com>
2025-05-15 00:29:10 +0300

    Update config.h

--- config.h
+++ config.h
@@ -5,9 +5,10 @@
 #define ENABLE_MAGNETIC_SENSOR 1
 
 // Enable or disable commander functionality
+// See: https://docs.simplefoc.com/commander_interface
 #define ENABLE_COMMANDER 0
 
-// Enable or disable angle reading
+// Enable or disable angle reading from TLE5012
 #define ENABLE_READ_ANGLE 0
 
 #endif // CONFIG_H
