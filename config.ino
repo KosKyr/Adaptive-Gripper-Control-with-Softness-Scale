6943e8a8c2228da531508d08a1ed86a97d6e0e10
KosKyr <90753277+KosKyr@users.noreply.github.com>
2025-05-14 16:20:49 +0200

    Update config.ino

--- config.ino
+++ config.ino
@@ -11,7 +11,7 @@
 #define MOTOR_PHASE_RESISTANCE 0.24f
 #define MOTOR_KV 360
 #define MOTOR_PHASE_INDUCTANCE 0.000133f
-#define MOTOR_VOLTAGE_ALIGN 2.0f
+#define MOTOR_VOLTAGE_ALIGN 4.0f
 #define MOTOR_POWER_SUPPLY_VOLTAGE 12.0f
 #define MOTOR_VOLTAGE_LIMIT 6.0f
 
@@ -30,14 +30,12 @@
 #define PIN_SPI1_SCK   68
 
 // === Button Pins ===
-#define BUTTON1 2
-#define BUTTON2 4
+//#define BUTTON1 2
+//#define BUTTON2 4
 #define MODE_SWITCH_PIN 7
 
-// === Magnetic Sensor Settings (TLx493D) ===
+// === Magnetic Sensor Settings ===
 #define CALIBRATION_SAMPLES 20
-#define GRIP_FIELD_THRESHOLD 0.6     // mT, total field magnitude
-#define GRIP_DELTA_THRESHOLD 0.2     // mT, rate of change
-#define FIELD_SAMPLE_INTERVAL 50     // ms
+#define FIELD_SAMPLE_INTERVAL 50  // milliseconds
 
-#endif
+#endif // CONFIG_H
