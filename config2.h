cd74a9ebaef7bb2fda6152777a2bd3e1c6367538
teolats <120052118+teolats@users.noreply.github.com>
2025-05-14 18:51:05 +0200

    Create config2.h

---
+++ config2.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Enable or disable magnetic sensor + button control
+#define ENABLE_MAGNETIC_SENSOR 1
+
+// Enable or disable commander functionality
+#define ENABLE_COMMANDER 0
+
+// Enable or disable angle reading
+#define ENABLE_READ_ANGLE 0
+
+#endif // CONFIG_H
