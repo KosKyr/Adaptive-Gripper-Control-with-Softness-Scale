9229e5788129c04e7de181fb04943e687244908d
KosKyr <90753277+KosKyr@users.noreply.github.com>
2025-05-14 19:40:18 +0200

    Update config.h

--- config.h
+++ config.h
@@ -1,19 +1,13 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Pin definitions for motor driver
-#define PIN_U       11
-#define PIN_V       10
-#define PIN_W       9
-#define PIN_EN_U    6
-#define PIN_EN_V    5
-#define PIN_EN_W    3
-
-// Buttons for gripper control
-#define BUTTON1     2  // Close gripper
-#define BUTTON2     4  // Open gripper
-
-// Enable/Disable magnetic sensor features
+// Enable or disable magnetic sensor + button control
 #define ENABLE_MAGNETIC_SENSOR 1
 
-#endif
+// Enable or disable commander functionality
+#define ENABLE_COMMANDER 0
+
+// Enable or disable angle reading
+#define ENABLE_READ_ANGLE 0
+
+#endif // CONFIG_H
